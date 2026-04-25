fdaf11f5187f982450b45d23a356eae1c94230b7
咸鱼 <lixiangyu951753@gmail.com>
2026-04-25 18:02:42 +0800

    chore: 更新Docker相关配置和测试脚本
    - 添加server.dockerignore文件忽略不必要的文件
    - 在Dockerfile中设置PYTHONUNBUFFERED环境变量
    - 添加docker镜像加速配置
    - 更新测试脚本中的API地址
    - 修改docker-compose配置，使用外部网络和指定容器名称

--- req_test_servers.py
+++ req_test_servers.py
@@ -1,5 +1,5 @@
 import requests
-r = requests.post('http://localhost:5000/api/tasks', json={
+r = requests.post('http://39.105.200.25/api/tasks', json={
     'instruction': 'start_crawl',
     'params': {'shop_url': 'https://xindeyi.1688.com/page/offerlist.htm', 'max_pages': 2}
 })
