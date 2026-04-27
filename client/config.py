92e13f5fd303c399e7d2039082aa3fb5549e90ac
咸鱼 <lixiangyu951753@gmail.com>
2026-04-27 10:08:35 +0800

    refactor(config): 更新客户端配置路径和服务端口
    统一修改客户端配置中的输出路径和服务端口，将client2的输出路径从"D:\\works\\carwl_client_server\\client2\\output"改为"D:\\client_002_output"，并将服务端口从5000改为5001。同时更新client1的输出路径为"D:\\client_001_output"

--- client/config.py
+++ client/config.py
@@ -4,7 +4,7 @@ CLIENT_ID = 'client_001'
 SERVER_URL = 'http://localhost:5001/api'
 HEARTBEAT_INTERVAL = 5
 
-BASE_PATH = r'D:\works\crawl\1688'
+BASE_PATH = r'D:\client_001_output'
 
 OSS_ACCESS_KEY_ID = os.environ.get('OSS_ACCESS_KEY_ID', '')
 OSS_ACCESS_KEY_SECRET = os.environ.get('OSS_ACCESS_KEY_SECRET', '')
